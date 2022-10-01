90047ae08f4a7ea1047f938a69dcc572a2b1476c
Benjamin Q <benking3502@yahoo.com>
2022-10-01 19:14:59 -0400

    Finished nearly all javascript
    Finished nearly all javascript except for showWinner function.

--- javascript.js
+++ javascript.js
@@ -5,17 +5,53 @@ const Gameboard = (() => {
     '', '', ''
   ];
   const checkWinner = () => {
-    // finish
+    if (
+      //Check rows for a winner
+      (gameboardArray[0] === gameboardArray[1]) && (gameboardArray[1] === gameboardArray[2]) && (gameboardArray[2] !== '') ||
+      (gameboardArray[3] === gameboardArray[4]) && (gameboardArray[4] === gameboardArray[5]) && (gameboardArray[5] !== '') ||
+      (gameboardArray[6] === gameboardArray[7]) && (gameboardArray[7] === gameboardArray[8]) && (gameboardArray[8] !== '') ||
+      //Check columns for a winner
+      (gameboardArray[0] === gameboardArray[3]) && (gameboardArray[3] === gameboardArray[6]) && (gameboardArray[6] !== '') ||
+      (gameboardArray[1] === gameboardArray[4]) && (gameboardArray[4] === gameboardArray[7]) && (gameboardArray[7] !== '') ||
+      (gameboardArray[2] === gameboardArray[5]) && (gameboardArray[5] === gameboardArray[8]) && (gameboardArray[8] !== '') ||
+      //Check diagonals for a winner
+      (gameboardArray[0] === gameboardArray[4]) && (gameboardArray[4] === gameboardArray[8]) && (gameboardArray[8] !== '') ||
+      (gameboardArray[2] === gameboardArray[4]) && (gameboardArray[4] === gameboardArray[6]) && (gameboardArray[6] !== '')
+    ) {
+      return 'winner';
+    } else {
+      // Loop through all cells. If there are no empty cells on the last iteration, its a tie.
+      let emptyCellsCount = 0;
+      for (let i = 0; i < 9; i++) {
+        if (gameboardArray[i] === '') {
+          emptyCellsCount++;
+        } else if (emptyCellsCount === 0 && i === 8) {
+          return 'tie';
+        }
+      }
+    }
+    return '';
   }
 
   const reset = () => {
     gameboardArray = ['', '', '', '', '', '', '', '', ''];
+    DisplayController.drawGameBoard();
   }
   const getGameboardArray = () => {
     return gameboardArray;
   }
   const placeMarker = (locationIndex) => {
-    // finish
+    if (gameboardArray[locationIndex] === '') {
+      gameboardArray[locationIndex] = Game.getCurrentPlayer().getMarker();
+      DisplayController.drawGameBoard();
+      let possibleWinner = checkWinner();
+      if (possibleWinner !== '') {
+        Game.endGame(possibleWinner);
+      } else {
+        Game.toggleCurrentPlayer();
+      }
+    }
+    // potentially display 'pick another slot' message
   }
 
   return {
@@ -59,38 +95,69 @@ const DisplayController = (() => {
   let player2NameDisplay = document.querySelector('.player2-name-display');
   let player2NameInput = document.querySelector('.player2-name-input');
 
+  const getPlayer1Name = () => {
+    return player1NameInput.value;
+  }
+  const getPlayer2Name = () => {
+    return player2NameInput.value;
+  }
   const updatePlayer1Name = () => {
     let newName = player1NameInput.value;
-    Gameboard.getPlayer1().setName(newName);
+    Game.getPlayer1().setName(newName);
     player1NameDisplay.textContent = newName;
   }
-
-  //CONTINUE
+  const updatePlayer2Name = () => {
+    let newName = player2NameInput.value;
+    Game.getPlayer2().setName(newName);
+    player2NameDisplay.textContent = newName;
+  }
+  const attachEventListeners = () => {
+    // Attach listeners to grid cells
+    let cells = document.querySelectorAll('.gameboard>.cell');
+    for (let cell of cells) {
+      cell.addEventListener('click', (e) => {
+        Gameboard.placeMarker(Number(e.target.getAttribute('location-index')));
+      });
+    }
+  }
+  const drawGameBoard = () => {
+    let gameboardArray = Gameboard.getGameboardArray();
+    let cells = document.querySelectorAll('.gameboard>.cell');
+    console.log(Gameboard.getGameboardArray());
+    for (let i = 0; i < 9; i++) {
+      cells[i].textContent = gameboardArray[i];
+    }
+  }
+  const showWinner = (possibleWinner) => {
+    // CONTINUE
+  }
 
   return {
-    gameboardElem,
-    player1NameDisplay,
-    player1NameInput,
-    player2NameDisplay,
-    player2NameInput,
+    getPlayer1Name,
+    getPlayer2Name,
     updatePlayer1Name,
+    updatePlayer2Name,
+    attachEventListeners,
+    drawGameBoard,
+    showWinner,
   };
 })();
 
-console.log(DisplayController.player2NameDisplay.textContent);
+// console.log(DisplayController.player2NameDisplay.textContent);
 
 const Game = (() => {
   let Player1 = {};
   let Player2 = {};
   let currentPlayer = {};
+  let lastWinner = {};
+
   const toggleCurrentPlayer = () => {
     if (currentPlayer === Player1) {
-      currentPlayer === Player2;
+      currentPlayer = Player2;
     } else {
-      currentPlayer === Player1;
+      currentPlayer = Player1;
     }
   };
-
   const setPlayer1 = (Player1Reference) => {
     Player1 = Player1Reference;
   }
@@ -103,25 +170,41 @@ const Game = (() => {
   const getPlayer2 = () => {
     return Player2;
   }
+  const getCurrentPlayer = () => {
+    return currentPlayer;
+  }
+  const getLastWinner = () => {
+    return lastWinner;
+  }
   const startGame = () => {
-    //NOT YET FUNCTIONING
-    const Player1 = Player(DisplayController.getPlayer1Name(), 'X');
-    const Player2 = Player(DisplayController.getPlayer2Name(), 'O');
-    
+    DisplayController.attachEventListeners();
+    Player1 = Player(DisplayController.getPlayer1Name(), 'X');
+    Player2 = Player(DisplayController.getPlayer2Name(), 'O');
     currentPlayer = Player1;
-    console.log(Player1.getName());
-    //DisplayController.drawGameBoard
+    lastWinner = {};
+    Gameboard.reset();
   };
-  const endGame = (PlayerWinner) => {
-    // DisplayController.showWinner(PlayerWinner)
+  const restartGame = () => {
+    Player1 = Player(DisplayController.getPlayer1Name(), 'X');
+    Player2 = Player(DisplayController.getPlayer2Name(), 'O');
+    Gameboard.reset();
+    currentPlayer = lastWinner;
+  }
+  const endGame = (possibleWinner) => {
+    lastWinner = currentPlayer;
+    DisplayController.showWinner(possibleWinner)
   }
 
   return {
+    toggleCurrentPlayer,
     setPlayer1,
     getPlayer1,
     setPlayer2,
     getPlayer2,
+    getCurrentPlayer,
+    getLastWinner,
     startGame,
+    restartGame,
     endGame,
   }
 })();
